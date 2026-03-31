6fc1257a357c224d6aff64fb4100226dcd036d89
iaremchuk <ya.olya24@gmail.com>
2026-03-31 19:43:00 +0100

    Add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { inter } from "@/lib/fonts/fonts";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 import "./globals.css";
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={`${inter.className} min-h-full flex flex-col`}>
         <Header />
         <main className="flex-1">{children}</main>
+        <Footer />
       </body>
     </html>
   );
